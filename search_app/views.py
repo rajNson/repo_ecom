d289d7168bcae7a1ddcb99dd4f10d5b5e867d4ed
sreerajcp <raj398309@gmail.com>
2023-06-27 23:14:33 +0530

    last commit

--- search_app/views.py
+++ search_app/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render
-from shop.models import Product, Category
-from django.db.models import Q
+# from django.shortcuts import render
+# from shop.models import Product, Category
+# from django.db.models import Q
+#
+# # Create your views here.
+#
+# def SearchResult(request):
+#     products = None
+#     query = None
+#     if 'q' in request.GET:
+#         query=request.GET.get('q')
+#         products=Product.objects.all().filter(Q(name__contains=query) | Q(description__contains=query))
+#         return render(request,'search.html',{'query':query,'products':products})
+#
 
-# Create your views here.
+from shop.models import Product
+from django.db.models import Q
+from django.shortcuts import render
+
 
 def SearchResult(request):
+    query = request.GET.get('q')
     products = None
-    query = None
-    if 'q' in request.GET:
-        query=request.GET.get('q')
-        products=Product.objects.all().filter(Q(name__contains=query) | Q(description__contains=query))
-        return render(request,'search.html',{'query':query,'products':products})
+
+    if query:
+        products = Product.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    return render(request, 'search.html', {'query': query, 'products': products})
+
